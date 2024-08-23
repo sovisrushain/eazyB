b2384c984dcbb78dc80e69e5db2cda9c9492dc6f
sovisrushain <sovisrushain@gmail.com>
2024-08-23 14:12:45 +0530

    create entities

---
+++ accounts/src/main/java/com/cisco/accounts/entity/Accounts.java
@@ -0,0 +1,22 @@
+package com.cisco.accounts.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+public class Accounts extends BaseEntity {
+    @Id
+    @Column(name = "accounts_number")
+    private Long accountNumber;
+    @Column(name = "customer_id")
+    private Long customerId;
+    @Column(name = "account_type")
+    private String accountType;
+    @Column(name = "branch_address")
+    private String branchAddress;
+}
