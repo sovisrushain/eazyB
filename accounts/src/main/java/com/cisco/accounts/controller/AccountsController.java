2c6d5b6f91adcd2bda3a49173112cb67d036e194
sovisrushain <sovisrushain@gmail.com>
2024-08-24 17:57:37 +0530

    initailize accounts constants

--- accounts/src/main/java/com/cisco/accounts/controller/AccountsController.java
+++ accounts/src/main/java/com/cisco/accounts/controller/AccountsController.java
@@ -1,13 +1,25 @@
 package com.cisco.accounts.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
+import com.cisco.accounts.constants.AccountsConstants;
+import com.cisco.accounts.dto.CustomerDTO;
+import com.cisco.accounts.dto.ResponseDTO;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping(params = "/api/v1/accounts", produces = MediaType.APPLICATION_JSON_VALUE)
 public class AccountsController {
 
-    @GetMapping
-    public String sayHello() {
-        return "Hello World!";
+    @PostMapping("/create")
+    public ResponseEntity<ResponseDTO> createAccount(@RequestBody CustomerDTO customerDTO) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(new ResponseDTO(
+                AccountsConstants.STATUS_201, AccountsConstants.MESSAGE_201
+        ));
     }
+
 }
