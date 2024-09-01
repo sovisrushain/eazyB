65e927f28519ca74f6328ce36672962e0e13fc53
sovisrushain <sovisrushain@gmail.com>
2024-09-01 23:54:12 +0530

    enhance documentation

--- accounts/src/main/java/com/cisco/accounts/controller/AccountsController.java
+++ accounts/src/main/java/com/cisco/accounts/controller/AccountsController.java
@@ -4,6 +4,10 @@ import com.cisco.accounts.constants.AccountsConstants;
 import com.cisco.accounts.dto.CustomerDTO;
 import com.cisco.accounts.dto.ResponseDTO;
 import com.cisco.accounts.service.AccountsService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.Pattern;
 import lombok.RequiredArgsConstructor;
@@ -16,11 +20,14 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequiredArgsConstructor
 @Validated
+@Tag(name = "REST API: Accounts")
 @RequestMapping(path = "/api/v1/accounts", produces = MediaType.APPLICATION_JSON_VALUE)
 public class AccountsController {
 
     private final AccountsService accountsService;
 
+    @Operation(summary = "REST API: Create Account")
+    @ApiResponse(responseCode = "201", description = "HTTP Status CREATED")
     @PostMapping("/create")
     public ResponseEntity<ResponseDTO> createAccount(@Valid @RequestBody CustomerDTO customerDTO) {
         accountsService.createAccount(customerDTO);
@@ -29,6 +36,8 @@ public class AccountsController {
         ));
     }
 
+    @Operation(summary = "REST API: Get Account details")
+    @ApiResponse(responseCode = "200", description = "HTTP Status OK")
     @GetMapping("/fetch")
     public ResponseEntity<CustomerDTO> fetchAccountDetails(@RequestParam
                                                                @Pattern(regexp="(^$|[0-9]{10})",message = "Mobile number must be 10 digits")
@@ -37,6 +46,11 @@ public class AccountsController {
         return ResponseEntity.status(HttpStatus.OK).body(customerDTO);
     }
 
+    @Operation(summary = "REST API: Update Account details")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "HTTP Status OK"),
+            @ApiResponse(responseCode = "500", description = "HTTP Status Internal Server Error")
+    })
     @PutMapping("/update")
     public ResponseEntity<ResponseDTO> updateAccountDetails(@Valid @RequestBody CustomerDTO customerDTO) {
         boolean isUpdated = accountsService.updateAccount(customerDTO);
@@ -51,6 +65,11 @@ public class AccountsController {
         }
     }
 
+    @Operation(summary = "REST API: Delete Account")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "HTTP Status OK"),
+            @ApiResponse(responseCode = "500", description = "HTTP Status Internal Server Error")
+    })
     @DeleteMapping("/delete")
     public ResponseEntity<ResponseDTO> deleteAccountDetails(@RequestParam
                                                             @Pattern(regexp="(^$|[0-9]{10})",message = "Mobile number must be 10 digits")
