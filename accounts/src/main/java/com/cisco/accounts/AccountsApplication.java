41eee529896278357cd2303e822dbce6e52e4a5c
sovisrushain <sovisrushain@gmail.com>
2024-09-01 23:43:12 +0530

    apply openapi definitions

--- accounts/src/main/java/com/cisco/accounts/AccountsApplication.java
+++ accounts/src/main/java/com/cisco/accounts/AccountsApplication.java
@@ -1,15 +1,39 @@
 package com.cisco.accounts;
 
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Accounts Service REST API",
+                description = "Bank microservice - Account service REST API documentation",
+                version = "v1",
+                contact = @Contact(
+                        name = "cisco",
+                        email = "sovisrushain@gmail.com",
+                        url = "https://sovisrushain.github.io/portfolio/"
+                ),
+                license = @License(
+                        name = "MIT"
+                )
+        ),
+        externalDocs = @ExternalDocumentation(
+                description = "Bank microservice - Account service REST API documentation",
+                url = "http://localhost:8081/swagger-ui/index.html"
+        )
+)
 public class AccountsApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(AccountsApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(AccountsApplication.class, args);
+    }
 
 }
